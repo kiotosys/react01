948a4bb168a7b502a0e06a683c6f91dcc1a84ade
Kioto <kiotosys@gmail.com>
2023-10-28 07:29:57 -0500

    v22.Funciona inicio de sesi sesión con Bootstrap y parte de React-Bootstrap

--- src/login/InicioSesion.js
+++ src/login/InicioSesion.js
@@ -24,31 +24,38 @@ function InicioSesion() {
       // Manejar errores de inicio de sesión, como credenciales incorrectas
     }
   }
+  
   return (
     <div>
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form className='card card-body'>
+      <Form.Group className="btn btn-primary btn-block" controlId="formBasicEmail">
         <Form.Label>Iniciar Sesión</Form.Label>
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Correo Electrónico" />
-        <Form.Text value={correo} onChange={(e) => setCorreo(e.target.value)} className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+      <Form.Label>Correo Electrónico</Form.Label>
+      
+      <Form.Group className="form-group input-group" controlId="formBasicEmail">
+        <div className="input-group-text bd-light">
+          <i className='material-icons'>group_add</i>
+        </div>
+        <input value={correo} onChange={(e) => setCorreo(e.target.value)} 
+          className='form-control float-start' type="email" placeholder="Correo Electrónico"  />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control value={contraseña} onChange={(e) => setContraseña(e.target.value)} type="password" placeholder="Password" />
+      <Form.Label>Password</Form.Label>
+      <Form.Group className="form-group input-group" controlId="formBasicPassword">
+        <div className="input-group-text bd-light">
+          <i className='material-icons'>group_add</i>
+        </div>
+        <input value={contraseña} onChange={(e) => setContraseña(e.target.value)} 
+        className='form-control float-start' type="password" placeholder="Contraseña"  />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
       </Form.Group>
       
-      <Button onClick={handleInicioSesion} variant="primary" type="submit">
+      <Button onClick={handleInicioSesion} variant="primary">
         Iniciar
       </Button>
     </Form>
